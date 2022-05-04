483546e1ed714e330820e8aa1d88303af77dc94e
Adam <nohbody5@gmail.com>
2022-05-04 13:38:35 -0600

    Progress

--- spider.py
+++ spider.py
@@ -11,6 +11,8 @@ REGEX string to collect additional data.
 '''
 
 import logging
+import requests
+import re
 
 # This allows for logs to work even if class is imported
 # all log events in this script should be called as
@@ -22,29 +24,78 @@ class spider():
         # Provide initial variables for scraping use
         
         # Active HTML object
+        self.html = None
         # URL Processing Queue
+        self.queue = set()
         # Processed URL list (for final Export)
+        self.visited = set()
         # current depth
+        self.depth = 0
         # Max Depth
+        self.maxdepth = 2
         # base URL for active HTML Object
+        self.baseURL = ''
             # For robots.txt tracking
             # Also for relative link buildout
-        pass
+        self.currentURL = ''
 
-    def crawl(self):
+        log.debug('Spider initialized')
+        log.debug('Initial variable values:')
+        log.debug(f'html: {self.html}')
+        log.debug(f'queue: {self.queue}')
+        log.debug(f'visited: {self.visited}')
+        log.debug(f'depth: {self.depth}')
+        log.debug(f'maxdepth: {self.maxdepth}')
+        log.debug(f'baseURL: {self.baseURL}')
+        log.debug(f'currentURL: {self.currentURL}')
+
+    def crawl(self, url):
         # process for navigating to and collecting new HTML object
         # should stop if maximum depth reached
-        pass
+        # should save HTML object to class
+        
+        # Extract base URL for relative URL rebuilding
+        #self.baseURL = re.
+
+        log.info(f'Requesting HTML for: {url}')
+        
+        self.html = requests.get(url)
+
+        log.debug('HTML collected with http status code' + \
+                  f' of {self.html.status_code}')
+        log.debug(f'Apparent encoding: {self.html.apparent_encoding}')
+
+        return None
 
     def scrape(self):
         # Process for reading and extracting data from HTML object
         # TODO Allow user to provide custom REGEX for custom data extraction
-        pass
+        
+
+
+        matches = re.findall(r'href=[\'|\"](\S*)[\'|\"]', self.html.text)
+        for x in matches:
+            try:
+                if x[1] == '/':
+                    self.queue.add(f'{self.baseURL}{x}')
+                    
+                elif x[1:4] == 'http':
+                    self.queue.add(x)
+                
+                else:
+                    pass
+            except IndexError:
+                pass
+
+        return None
 
     def robot(self):
         # process to call in and remove robots.txt values from processing queue
         pass
 
+    def report(self):
+        # Method to do final export of collected data
+        pass
 
 if __name__ == '__main__':
     # Driver code for local testing
@@ -57,3 +108,13 @@ if __name__ == '__main__':
     
     # Log to terminal for testing
     logging.basicConfig(level='DEBUG')
+
+    # Hardcode starting URL for testing
+    startURL = 'https://google.com'
+
+    # Spider instance for testing
+    s = spider()
+
+    # TODO: Eventually this will need to be wrapped in a while loop
+    # so that it can continue processing until we identify an exit point
+    s.crawl(startURL)
